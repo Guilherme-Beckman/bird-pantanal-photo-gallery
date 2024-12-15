c321e9d08300f3a8b485b9502c9651ef6c131d20
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2024-12-15 15:34:03 -0400

    feat: finish the photo-cards

--- frontend/bird-pantanal-photo-gallery/src/app/app.component.ts
+++ frontend/bird-pantanal-photo-gallery/src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { PhotoCardsComponent } from './components/photo-cards/photo-cards.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, NavbarComponent],
+    imports: [RouterOutlet, NavbarComponent, PhotoCardsComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
