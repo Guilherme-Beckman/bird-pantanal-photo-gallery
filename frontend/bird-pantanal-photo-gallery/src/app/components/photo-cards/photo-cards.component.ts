a68bb9ae96d9b1030088818d8b935b54f06e820d
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2024-12-15 19:27:43 -0400

    feat: finish the read-more feature

--- frontend/bird-pantanal-photo-gallery/src/app/components/photo-cards/photo-cards.component.ts
+++ frontend/bird-pantanal-photo-gallery/src/app/components/photo-cards/photo-cards.component.ts
@@ -1,11 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-photo-cards',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './photo-cards.component.html',
   styleUrl: './photo-cards.component.scss'
 })
 export class PhotoCardsComponent {
-
+  showMore = false;
+  showMoreInfo(){
+    this.showMore = !this.showMore;
+  }
 }
