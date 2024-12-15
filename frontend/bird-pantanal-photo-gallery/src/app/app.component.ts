bf5a585098949a792a01f5418d97fda3cc348c3f
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2024-12-15 16:41:48 -0400

    feat: finish the footer

--- frontend/bird-pantanal-photo-gallery/src/app/app.component.ts
+++ frontend/bird-pantanal-photo-gallery/src/app/app.component.ts
@@ -4,10 +4,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SummaryComponent } from './components/summary/summary.component';
 import { SocialMediaSectionComponent } from './components/social-media-section/social-media-section.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, NavbarComponent, PhotoCardsComponent, SummaryComponent, SocialMediaSectionComponent],
+    imports: [RouterOutlet, NavbarComponent, PhotoCardsComponent, SummaryComponent, SocialMediaSectionComponent, FooterComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
