e1cedf866844b158865fe5b90c98471329cc9597
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2024-12-15 13:49:21 -0400

    feat: generate photo-card component

---
+++ frontend/bird-pantanal-photo-gallery/src/app/components/photo-cards/photo-cards.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-cards',
+  imports: [],
+  templateUrl: './photo-cards.component.html',
+  styleUrl: './photo-cards.component.scss'
+})
+export class PhotoCardsComponent {
+
+}
