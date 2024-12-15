ae69a1228c7066199d816b127e381806fdd8eba2
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2024-12-15 15:59:30 -0400

    feat: finish the sumarry

--- frontend/bird-pantanal-photo-gallery/src/app/app.component.ts
+++ frontend/bird-pantanal-photo-gallery/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { PhotoCardsComponent } from './components/photo-cards/photo-cards.compon
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SummaryComponent } from './components/summary/summary.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, NavbarComponent, PhotoCardsComponent],
+    imports: [RouterOutlet, NavbarComponent, PhotoCardsComponent, SummaryComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
