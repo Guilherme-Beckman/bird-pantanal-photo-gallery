d447450f4887aac25d2afc47819bdfc82edc3d60
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2024-12-16 15:13:34 -0400

    feat: create the loadMore logic and fix: read more info

--- frontend/bird-pantanal-photo-gallery/src/app/components/photo-cards/photo-cards.component.ts
+++ frontend/bird-pantanal-photo-gallery/src/app/components/photo-cards/photo-cards.component.ts
@@ -8,8 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './photo-cards.component.scss'
 })
 export class PhotoCardsComponent {
-  showMore = false;
-  showMoreInfo(){
-    this.showMore = !this.showMore;
+  cards = [
+    { title: 'Card 1', image: 'assets/tuiuiu.jpg', alt: 'Description of the image', description: 'Description for Card 1', showMore: false },
+    { title: 'Card 2', image: 'assets/tuiuiu.jpg', alt: 'Description of the image', description: 'Description for Card 2', showMore: false },
+    { title: 'Card 3', image: 'assets/tuiuiu.jpg', alt: 'Description of the image', description: 'Description for Card 3', showMore: false },
+    { title: 'Card 4', image: 'assets/tuiuiu.jpg', alt: 'Description of the image', description: 'Description for Card 4', showMore: false },
+    { title: 'Card 5', image: 'assets/tuiuiu.jpg', alt: 'Description of the image', description: 'Description for Card 5', showMore: false },
+    { title: 'Card 6', image: 'assets/tuiuiu.jpg', alt: 'Description of the image', description: 'Description for Card 6', showMore: false },
+    { title: 'Card 7', image: 'assets/tuiuiu.jpg', alt: 'Description of the image', description: 'Description for Card 7', showMore: false },
+    { title: 'Card 8', image: 'assets/tuiuiu.jpg', alt: 'Description of the image', description: 'Description for Card 8', showMore: false },
+  ]
+  visibleCards = this.cards.slice(0,6);
+
+  showMoreInfo(index: number){
+    this.visibleCards[index].showMore = !this.visibleCards[index].showMore;
+  } 
+  loadMore(){
+    const nextCards = this.cards.slice(this.visibleCards.length, this.visibleCards.length + 6);
+    this.visibleCards = [...this.visibleCards, ...nextCards]
   }
 }
