9faa012950e4d3857671e991e0c6a2a680a926de
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2024-12-17 13:27:22 -0400

    init spring project

---
+++ backend/bird-pantanal-photo-gallery/src/main/java/com/projects/bird_pantanal_photo_gallery/BirdPantanalPhotoGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.projects.bird_pantanal_photo_gallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdPantanalPhotoGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdPantanalPhotoGalleryApplication.class, args);
+	}
+
+}
