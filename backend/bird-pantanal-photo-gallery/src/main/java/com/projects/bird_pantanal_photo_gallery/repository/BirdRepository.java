02d7f23c5da46fc440fd1b3d23bd579613a049d7
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2024-12-20 14:53:01 -0400

    feat: add a repository for birds

---
+++ backend/bird-pantanal-photo-gallery/src/main/java/com/projects/bird_pantanal_photo_gallery/repository/BirdRepository.java
@@ -0,0 +1,10 @@
+package com.projects.bird_pantanal_photo_gallery.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.projects.bird_pantanal_photo_gallery.model.BirdModel;
+@Repository
+public interface BirdRepository extends JpaRepository<BirdModel, String>{
+
+}
