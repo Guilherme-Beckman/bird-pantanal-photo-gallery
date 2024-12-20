e2ba5e9a444eda0d8ba9a9a49482959f2bc39d3c
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2024-12-20 11:39:44 -0400

    feat: add a method in StorageController to delete files

--- backend/bird-pantanal-photo-gallery/src/main/java/com/projects/bird_pantanal_photo_gallery/infra/aws/controller/StorageController.java
+++ backend/bird-pantanal-photo-gallery/src/main/java/com/projects/bird_pantanal_photo_gallery/infra/aws/controller/StorageController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,5 +45,10 @@ public class StorageController {
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
                 .body(fileContent);
+    }
+    @DeleteMapping("/delete/{fileName}")
+    public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
+        String responseMessage = storageService.deleteFile(fileName);
+        return ResponseEntity.ok(responseMessage);
     }
 }
