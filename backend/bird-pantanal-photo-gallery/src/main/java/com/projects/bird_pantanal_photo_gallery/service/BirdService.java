ce0b23af5579061846f153b58cfc1422d65c0eef
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2024-12-20 15:13:56 -0400

    create a createBird in BirdService

--- backend/bird-pantanal-photo-gallery/src/main/java/com/projects/bird_pantanal_photo_gallery/service/BirdService.java
+++ backend/bird-pantanal-photo-gallery/src/main/java/com/projects/bird_pantanal_photo_gallery/service/BirdService.java
@@ -3,7 +3,8 @@ package com.projects.bird_pantanal_photo_gallery.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+import com.projects.bird_pantanal_photo_gallery.model.BirdModel;
+import com.projects.bird_pantanal_photo_gallery.model.dto.BirdDTO;
 import com.projects.bird_pantanal_photo_gallery.repository.BirdRepository;
 
 @Service
@@ -11,5 +12,8 @@ public class BirdService {
 	@Autowired
 	private BirdRepository birdRepository;
 	
-
+	public BirdModel createBird(BirdDTO birdDTO) {
+		BirdModel newBird = new BirdModel(birdDTO);
+		return this.birdRepository.save(newBird);
+	}
 }
