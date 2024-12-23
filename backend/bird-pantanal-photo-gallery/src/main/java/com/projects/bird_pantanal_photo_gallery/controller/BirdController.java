8a3d444a0547e462ce3de266e073e84ea0426374
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2024-12-23 11:57:42 -0400

    finish the exception treatment

--- backend/bird-pantanal-photo-gallery/src/main/java/com/projects/bird_pantanal_photo_gallery/controller/BirdController.java
+++ backend/bird-pantanal-photo-gallery/src/main/java/com/projects/bird_pantanal_photo_gallery/controller/BirdController.java
@@ -1,7 +1,6 @@
 package com.projects.bird_pantanal_photo_gallery.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -12,11 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.projects.bird_pantanal_photo_gallery.model.BirdModel;
 import com.projects.bird_pantanal_photo_gallery.model.dto.BirdDTO;
 import com.projects.bird_pantanal_photo_gallery.service.BirdService;
--- backend/bird-pantanal-photo-gallery/src/main/java/com/projects/bird_pantanal_photo_gallery/model/BirdModel.java
+++ backend/bird-pantanal-photo-gallery/src/main/java/com/projects/bird_pantanal_photo_gallery/model/BirdModel.java
@@ -1,16 +1,13 @@
 package com.projects.bird_pantanal_photo_gallery.model;
 
 import java.io.Serializable;
-
 import com.projects.bird_pantanal_photo_gallery.model.dto.BirdDTO;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
