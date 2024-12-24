2bb94719a9617cccbd83048e10322e54e9963000
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2024-12-24 16:02:36 -0400

    feat: Enable Caching

--- backend/bird-pantanal-photo-gallery/src/main/java/com/projects/bird_pantanal_photo_gallery/BirdPantanalPhotoGalleryApplication.java
+++ backend/bird-pantanal-photo-gallery/src/main/java/com/projects/bird_pantanal_photo_gallery/BirdPantanalPhotoGalleryApplication.java
@@ -2,8 +2,10 @@ package com.projects.bird_pantanal_photo_gallery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class BirdPantanalPhotoGalleryApplication {
 
 	public static void main(String[] args) {
