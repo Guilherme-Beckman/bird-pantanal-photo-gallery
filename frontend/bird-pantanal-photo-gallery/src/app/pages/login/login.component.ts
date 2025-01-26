f55c392f94480e429a68bacd039a96b7b7c36997
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-01-26 19:43:11 -0400

    feat: add success and error messages to login component

--- frontend/bird-pantanal-photo-gallery/src/app/pages/login/login.component.ts
+++ frontend/bird-pantanal-photo-gallery/src/app/pages/login/login.component.ts
@@ -1,27 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth/auth.service';
 import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, FormsModule],
+  imports: [ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   email: string = '';
   password: string ='';
+  successMessage: string = '';
+  errorMessage: string = '';
+  isLoading: boolean = false;
 
   constructor(private authService: AuthService){}
 
   onLogin():void{
     this.authService.login(this.email, this.password).subscribe({
       next: (response) =>{
+        this.isLoading = false;
         this.authService.saveToken(response.token);
-        console.log('Login bem-sucedido, token armazenado!');
+        this.successMessage = 'Login bem-sucedido, token armazenado!';
+        this.errorMessage = '';
+        setTimeout(() => {
+          this.successMessage = '';
+        }, 2000);
       },
       error: (error) => {
-        console.error('Erro ao fazer login', error);
+        this.isLoading = false;
+        this.errorMessage = 'Erro ao fazer login' + error.error.detail;
+        this.successMessage = '';
+        setTimeout(() => {
+          this.errorMessage = '';
+        }, 2000);
       },
       complete: () => {
         console.log('Requisição de login completa.');
