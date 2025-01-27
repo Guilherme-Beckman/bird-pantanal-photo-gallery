0d47e3f6a03e97622cb1e69b9f056f8dfe624a69
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-01-27 14:38:30 -0400

    feat: enhance login form with email and password fields and submission handling

--- frontend/bird-pantanal-photo-gallery/src/app/components/login-form/login-form.component.ts
+++ frontend/bird-pantanal-photo-gallery/src/app/components/login-form/login-form.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss'
 })
 export class LoginFormComponent {
-
+  @Input() isLoading: boolean =false;
+  @Output() loginSubmit = new EventEmitter<{ email: string, password: string }>();
+  email: string = '';
+  password: string = '';
+  onSubmit() {
+    this.loginSubmit.emit({ email: this.email, password: this.password });
+  }
 }
