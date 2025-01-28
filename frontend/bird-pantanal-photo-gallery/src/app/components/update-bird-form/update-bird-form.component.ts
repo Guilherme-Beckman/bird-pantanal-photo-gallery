c6445853cb44f9ae3f3ce487f816ba9be505f809
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-01-28 12:19:00 -0400

    feat: add update bird form component with initial structure and test setup

---
+++ frontend/bird-pantanal-photo-gallery/src/app/components/update-bird-form/update-bird-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-bird-form',
+  imports: [],
+  templateUrl: './update-bird-form.component.html',
+  styleUrl: './update-bird-form.component.scss'
+})
+export class UpdateBirdFormComponent {
+
+}
