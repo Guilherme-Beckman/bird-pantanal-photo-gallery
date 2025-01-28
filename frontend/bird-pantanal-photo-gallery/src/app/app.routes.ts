e32ba1feeb0cf6d529950c795e83a39ced7c9ede
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-01-28 19:51:29 -0400

    feat: implement auth guard for create and update routes

--- frontend/bird-pantanal-photo-gallery/src/app/app.routes.ts
+++ frontend/bird-pantanal-photo-gallery/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CreateBirdComponent } from './pages/create-bird/create-bird.component';
 import { UpdateBirdComponent } from './pages/update-bird/update-bird.component';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [ { path: 'login', component: LoginComponent},{ path: '', component: HomeComponent},{path: 'create', component: CreateBirdComponent}, 
-    {path: 'update', component: UpdateBirdComponent}];
+export const routes: Routes = [ { path: 'login', component: LoginComponent},{ path: '', component: HomeComponent},{path: 'create', component: CreateBirdComponent, canActivate: [authGuard]}, 
+    {path: 'update', component: UpdateBirdComponent, canActivate: [authGuard]}];
--- frontend/bird-pantanal-photo-gallery/src/app/guards/auth.guard.ts
+++ frontend/bird-pantanal-photo-gallery/src/app/guards/auth.guard.ts
@@ -1,5 +1,16 @@
 import { CanActivateFn } from '@angular/router';
+import { AuthService } from '../services/auth/auth.service';
+import { Router } from '@angular/router';
+import { inject } from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  
+  if (authService.isAuthenticated()) {
+    return true; 
+  } else {
+    router.navigate(['/login']); 
+    return false; 
+  }
 };
