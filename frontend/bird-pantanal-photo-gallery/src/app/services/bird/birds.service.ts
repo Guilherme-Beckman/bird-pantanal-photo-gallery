82aecf1b97b0771de262a883a726190ba9ad249c
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-02-01 20:00:15 -0400

    feat: update API URLs in AuthService and BirdsService for Docker compatibility

--- frontend/bird-pantanal-photo-gallery/src/app/services/bird/birds.service.ts
+++ frontend/bird-pantanal-photo-gallery/src/app/services/bird/birds.service.ts
@@ -8,7 +8,7 @@ import { BirdDTO } from '../../dto/bird.dto';
   providedIn: 'root'
 })
 export class BirdsService {
-  private apiUrl = 'http://localhost:8080/birds/';
+  private apiUrl = 'http://host.docker.internal:8080/birds/';
   constructor(private httpClient:HttpClient) { }
   
   getBirdById(id: string){
