4901fe515eafca83d261ffc4769f8d7289db6872
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-02-03 17:28:39 -0400

    feat: update Vite configuration to include additional allowed host for server

--- frontend/bird-pantanal-photo-gallery/vite.config.js
+++ frontend/bird-pantanal-photo-gallery/vite.config.js
@@ -1,6 +1,10 @@
-export default {
-  server: {
-    allowedHosts: ['passarosdopantanal.com.br'],
-  },
-};
+// vite.config.js
+import { defineConfig } from 'vite';
+import angular from '@analogjs/vite-plugin-angular';
 
+export default defineConfig({
+  plugins: [angular()],
+  server: {
+    allowedHosts: ['passarosdopantanal.com.br', 'www.passarosdopantanal.com.br'] // Add your domain here
+  }
+});
