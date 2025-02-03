6812c78973bc95efa020aa4e11cbab5c07d39eab
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-02-03 12:15:25 -0400

    feat: update API URLs in AuthService and BirdsService to new server address

--- frontend/bird-pantanal-photo-gallery/src/app/services/auth/auth.service.ts
+++ frontend/bird-pantanal-photo-gallery/src/app/services/auth/auth.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://54.173.220.207:8080/auth/login';
+  private apiUrl = 'http://3.86.191.231:8080/auth/login';
 
   constructor(private httpClient: HttpClient) {}
 
--- frontend/bird-pantanal-photo-gallery/src/app/services/bird/birds.service.ts
+++ frontend/bird-pantanal-photo-gallery/src/app/services/bird/birds.service.ts
@@ -8,7 +8,7 @@ import { BirdDTO } from '../../dto/bird.dto';
   providedIn: 'root'
 })
 export class BirdsService {
-  private apiUrl = 'http://54.173.220.207:8080/birds/';
+  private apiUrl = 'http://3.86.191.231:8080/birds/';
   constructor(private httpClient:HttpClient) { }
   
   getBirdById(id: string){
